5ff2a538a82ea90172752b3925e33cdaac79b9c2
Hjalte <h.jaltehb123@gmail.com>
2020-10-08 15:48:20 +0200

    all inputs

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,13 @@ MPU6050 mpu6050(Wire);
 
 int joystickXPin = 2;
 int joystickYPin = 4;
+int potentiometerPin = 15;
 
 void setup() {
   Serial.begin(9600);
   pinMode(joystickXPin, INPUT);
   pinMode(joystickYPin, INPUT);
+  pinMode(potentiometerPin, INPUT);
 
   Wire.begin();
   mpu6050.begin();
@@ -19,15 +21,17 @@ void setup() {
 }
 
 void loop() {
+  Serial.print("Throttle: ");
+  Serial.print(analogRead(potentiometerPin));
   
-  Serial.print("X: ");
+  Serial.print(", Yaw: ");
   Serial.print(analogRead(joystickXPin));
 
-  // mpu6050.update();
-  // Serial.print("angleX : ");
-  // Serial.print(mpu6050.getAngleX());
-  // Serial.print("\tangleY : ");
-  // Serial.print(mpu6050.getAngleY());
+  mpu6050.update();
+  Serial.print(", Roll : ");
+  Serial.print(mpu6050.getAngleX());
+  Serial.print(", Pitch : ");
+  Serial.println(mpu6050.getAngleY());
   
 
 }
