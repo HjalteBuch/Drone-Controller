45a27bca9a1decc6668c50d53dbd0fbcc795f28f
Hjalte <h.jaltehb123@gmail.com>
2020-10-15 10:50:07 +0200

    Cleanup in loop

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,26 @@ MPU6050 mpu6050(Wire);
 
 AsyncUDP udp;
 //wifi name
-const char * ssid = "TELLO-598E42";
-const char * password = "";
+const char *ssid = "TELLO-598E42";
+const char *password = "";
 
+//Pin numbers bliver defineret her
 const int joystickXPin = 2;
 const int joystickYPin = 36;
 const int potentiometerPin = 32;
 const int buttonPinWhite = 23;
 const int buttonPinBlue = 5;
 
+//til potentiometeret
+bool prevalue;
 
-
+//takeoff / land check
 boolean inAir = false;
 
 int takeofflandcheck = 0;
 
-void command(){
+void command()
+{
   String msg = "command";
   udp.writeTo((const uint8_t *)msg.c_str(), msg.length(),
               IPAddress(192, 168, 10, 1), 8889);
@@ -31,20 +35,28 @@ void command(){
 
 //virker kun første gang
 //hvis dronen bliver disconnected, kan den ikke connecte til dronen igen.
-void connect(){
-  Serial.println("Connecting...");
-  WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("WiFi Failed");
-    return;
+void connect()
+{
+  if (digitalRead(buttonPinBlue) == LOW)
+  {
+    Serial.println("Connecting...");
+    WiFi.begin(ssid, password);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
+      Serial.println("WiFi Failed");
+      return;
+    }
+    if (WiFi.waitForConnectResult() == WL_CONNECTED)
+    {
+      command();
+      Serial.println("Connected");
+    }
   }
-  if (WiFi.waitForConnectResult() == WL_CONNECTED) {
-    command();
-    Serial.println("Connected");
-  }
+  delay(1000);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(joystickXPin, INPUT);
   pinMode(joystickYPin, INPUT);
@@ -61,65 +73,69 @@ void setup() {
   Serial.println("ready!");
 }
 
-void takeoff(){
+void takeoff()
+{
   String msg = "takeoff";
   udp.writeTo((const uint8_t *)msg.c_str(), msg.length(),
               IPAddress(192, 168, 10, 1), 8889);
 }
 
-void land(){
+void land()
+{
   String msg = "land";
   udp.writeTo((const uint8_t *)msg.c_str(), msg.length(),
               IPAddress(192, 168, 10, 1), 8889);
 }
 
-
-//når potentiometer står stille køre den skiftevis "descending" og "ascending", men den fatter mens man drejer
-int prev = analogRead(potentiometerPin);
-void upAndDown(){
-  int ascend = analogRead(potentiometerPin) / 9 + 20;
-
-  if(ascend < prev-2){
-    String msg = "down" + ascend;
-    udp.writeTo((const uint8_t *)msg.c_str(), msg.length(),
-              IPAddress(192, 168, 10, 1), 8889);
-    Serial.println("descending");
-  }
-
-  if(ascend > prev+2){
-    String msg = "up" + ascend;
-    udp.writeTo((const uint8_t *)msg.c_str(), msg.length(),
-              IPAddress(192, 168, 10, 1), 8889);
-    Serial.println("ascending");
-  }
-  
-  prev = ascend;
-}
-
-void loop() {
-
-  if(digitalRead(buttonPinBlue) == LOW){
-    connect();
-    delay(1000);
-
-  }
-  
-  while(digitalRead(buttonPinWhite) == LOW){
-    if(!inAir){
+void takeoffLand()
+{
+  while (digitalRead(buttonPinWhite) == LOW)
+  {
+    if (!inAir)
+    {
       takeoff();
       inAir = true;
       Serial.println("Takeoff");
       delay(1000);
       break;
     }
-    if(inAir){
-     land();
-     inAir = false;
+    if (inAir)
+    {
+      land();
+      inAir = false;
       Serial.println("Land");
       delay(1000);
       break;
     }
   }
-  upAndDown();
+}
 
+//når potentiometer står stille køre den skiftevis "descending" og "ascending", men den fatter mens man drejer
+int prev = analogRead(potentiometerPin);
+void upAndDown()
+{
+  int ascend = analogRead(potentiometerPin);
+
+  if (ascend < prev - 1)
+  {
+    String msg = "down " + ascend;
+    udp.writeTo((const uint8_t *)msg.c_str(), msg.length(),
+                IPAddress(192, 168, 10, 1), 8889);
+  }
+
+  if (ascend > prev + 1)
+  {
+    String msg = "up " + ascend;
+    udp.writeTo((const uint8_t *)msg.c_str(), msg.length(),
+                IPAddress(192, 168, 10, 1), 8889);
+  }
+
+  prev = ascend;
+}
+
+void loop()
+{
+  connect();
+  takeoffLand();
+  upAndDown();
 }
